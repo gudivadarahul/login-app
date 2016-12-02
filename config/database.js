9745e85d3e183fd4a880adaf0350430b31955b83
gudivadarahul <gudivadarahul@gmail.com>
2016-12-02 15:28:36 -0800

    god please

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://gudivadarahul:rahul123g@jello.modulusmongo.net:27017/y6jumeTy' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
